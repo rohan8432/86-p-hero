359c2d0473129cb1586919397816fbd004cc5197
Tafsirul islam Rohan <mdrohan8432@gmail.com>
2023-09-01 21:58:55 +0600

    added views

--- ph.js
+++ ph.js
@@ -1,9 +1,9 @@
-const handleCoding = async()=>{
+const handleCoding = async () => {
     const response = await fetch('https://openapi.programming-hero.com/api/videos/categories');
     const data = await response.json();
     console.log(data.data);
     const tabContainer = document.getElementById('tab-container');
-    data.data.forEach((category)=>{
+    data.data.forEach((category) => {
         const div = document.createElement('div');
         div.innerHTML = `
         <a onclick="loadCodingPost(${category.category_id})" class="tab">${category.category}</a>
@@ -13,40 +13,43 @@ const handleCoding = async()=>{
     })
 
 }
-const loadCodingPost= async(categoryId)=>{
-    const response =await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`)
+const loadCodingPost = async (categoryId) => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`)
     const data = await response.json();
 
 
 
-  const cardContainer = document.getElementById('card-container');
+    const cardContainer = document.getElementById('card-container');
 
- data.data.forEach((entertainmentPost)=>{
-    console.log(entertainmentPost);
-    
-    const div = document.createElement('div');
-    div.innerHTML = `
+    data.data.forEach((entertainmentPost) => {
+        console.log(entertainmentPost);
+
+        const div = document.createElement('div');
+        div.innerHTML = `
     <div class="card w-96 bg-base-100 shadow-xl">
-    <figure><img src="${entertainmentPost.thumbnail}" alt="Shoes" /></figure>
+    <figure><img class= "h-56 w-80" src="${entertainmentPost.thumbnail}" alt="Shoes" /></figure>
+    
     <div class="card-body">
-      <h2 class="card-title">
-       ${entertainmentPost.title}
-        <div class="badge badge-secondary">NEW</div>
-      </h2>
-      <p>If a dog chews shoes whose shoes does he choose?</p>
-      <div class="card-actions justify-end">
-        <div class="badge badge-outline">Fashion</div> 
-        <div class="badge badge-outline">Products</div>
-      </div>
+    <div class="flex gap-5">
+                <div>
+                    <img class="rounded-full h-12" src="${entertainmentPost.authors[0].profile_picture}" alt="">
+                   </div>
+                   <h2 class="card-title">
+                   ${entertainmentPost.title}
+                  </h2>
+               </div>
+      <p class="text-sm text-gray-500">${entertainmentPost.authors[0].profile_name}</p>
+      <p class="text-sm text-gray-500">${entertainmentPost.others.views} views</p>
+      
     </div>
   </div>
     `
-    cardContainer.appendChild(div);
- })
+        cardContainer.appendChild(div);
+    })
 
 
-  
-  
-  
+
+
+
 }
 handleCoding();
